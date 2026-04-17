168f45c5f874821a413089e468f4eb75b3194d33
Fakhri17 <fakhricoffe@gmail.com>
2026-04-17 22:42:32 +0700

    Add full name rendering in test view

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -11,8 +11,13 @@ final class TestController extends AbstractController
     #[Route("/test", name: "app_test")]
     public function index(): Response
     {
+        $first_name = "Fakhri";
+        $last_name = "Alauddin";
+
         return $this->render("test/index.html.twig", [
             "controller_name" => "TestController",
+            "first_name" => $first_name,
+            "last_name" => $last_name,
         ]);
     }
 }
